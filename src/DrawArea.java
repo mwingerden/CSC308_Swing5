30302d94577989fc34ba3fba3ae60302719d969a
Matthew Wingerden <matthew.wingerden@gmail.com>
2023-01-30 13:33:37 -0800

    Added mouse events.

--- src/DrawArea.java
+++ src/DrawArea.java
@@ -1,9 +1,58 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class DrawArea extends JPanel {
+public class DrawArea extends JPanel implements MouseListener, MouseMotionListener {
+    int x1, y1,x2, y2;
+
+    public DrawArea() {
+        addMouseListener(this);
+        addMouseMotionListener(this);
+    }
+
     public void paintComponent(Graphics g) {
         g.setColor(Color.RED);
         g.fillRect(10, 10, 100, 100);
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        x1 = e.getX();
+        y1 = e.getY();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        x2 = e.getX();
+        y2 = e.getY();
+        System.out.println(x1 + ", " + y1 + ", " + x2 + ", " + y2);
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
     }
 }
